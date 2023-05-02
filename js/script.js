96e86893eeefaac3b047355182bf75be8a500342
SimoneRicco <smnrcc0@gmail.com>
2023-05-02 16:09:33 +0200

    text not working

--- js/script.js
+++ js/script.js
@@ -7,27 +7,27 @@
 // 	'https://www.freecodecamp.org/news/content/images/2022/08/pexels-antonio-batinic--4164418--1-.jpg'
 // ];
 const images = [
-    {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
+	{
+		image: 'img/01.webp',
+		title: 'Marvel\'s Spiderman Miles Morale',
+		text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+	}, {
+		image: 'img/02.webp',
+		title: 'Ratchet & Clank: Rift Apart',
+		text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+	}, {
+		image: 'img/03.webp',
+		title: 'Fortnite',
+		text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+	}, {
+		image: 'img/04.webp',
+		title: 'Stray',
+		text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+	}, {
+		image: 'img/05.webp',
+		title: "Marvel's Avengers",
+		text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+	}
 ];
 
 const containerHighlighted = document.querySelector('.highlighted');
@@ -35,7 +35,7 @@ const containerThumbs = document.querySelector('.thumbs');
 
 for (let i = 0; i < images.length; i++) {
 	console.log(images[i].image)
-	containerHighlighted.innerHTML += `<img src="${images[i].image}" alt="" class="${i == 0 ? 'active' : ''}">`;
+	containerHighlighted.innerHTML += `<img src="${images[i].image}" alt="" class= "${i == 0 ? 'active' : ''}">`;
 	containerThumbs.innerHTML += `<img src="${images[i].image}" alt="" class="${i == 0 ? 'active' : ''}">`;
 }
 
@@ -49,46 +49,45 @@ const btnPrev = document.querySelector('.btn-prev');
 const btnNext = document.querySelector('.btn-next');
 
 
-// definito una variabile che rappresenta lo stato attuale del carosello
-// cioe' l'indice dell'immagine attiva
 let activeIndex = 0;
-
-btnNext.addEventListener('click',
-	function() {
-		// dall'immagine attiva tolgo la classe active
-		listHighlighted[activeIndex].classList.remove('active');
-		listThumbs[activeIndex].classList.remove('active');
-		// settiamo il nuovo valore di active index
-		activeIndex++;
-		if (activeIndex >= listHighlighted.length) {
-			activeIndex = 0;
-		}
-		// alla nuova immagine attiva aggiungiamo la classe active
-		listHighlighted[activeIndex].classList.add('active');
-		listThumbs[activeIndex].classList.add('active');
+btnNext.addEventListener('click', next);
+containerHighlighted.innerHTML += `<div class = text-hihlighted> ${write(activeIndex)} </div>`;
+const caroselText = document.querySelector(".text-highlighted");
+function next() {
+	// dall'immagine attiva tolgo la classe active
+	listHighlighted[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
+	// settiamo il nuovo valore di active index
+	activeIndex++;
+	if (activeIndex >= listHighlighted.length) {
+		activeIndex = 0;
 	}
-);
+	// alla nuova immagine attiva aggiungiamo la classe active
+	listHighlighted[activeIndex].classList.add('active');
+	listThumbs[activeIndex].classList.add('active');
+	caroselText.innerHTML = write(activeIndex);
+}
 
-btnPrev.addEventListener('click',
-	function() {
-		// dall'immagine attiva tolgo la classe active
-		listHighlighted[activeIndex].classList.remove('active');
-		listThumbs[activeIndex].classList.remove('active');
-		// settiamo il nuovo valore di active index
-		activeIndex--;
-		if (activeIndex < 0) {
-			activeIndex = listHighlighted.length - 1;
-		}
-		// alla nuova immagine attiva aggiungiamo la classe active
-		listHighlighted[activeIndex].classList.add('active');
-		listThumbs[activeIndex].classList.add('active');
+function prev() {
+	// dall'immagine attiva tolgo la classe active
+	listHighlighted[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
+	// settiamo il nuovo valore di active index
+	activeIndex--;
+	if (activeIndex < 0) {
+		activeIndex = listHighlighted.length - 1;
 	}
-);
+	// alla nuova immagine attiva aggiungiamo la classe active
+	listHighlighted[activeIndex].classList.add('active');
+	listThumbs[activeIndex].classList.add('active');
+	caroselText.innerHTML = write(activeIndex);
+}
+btnPrev.addEventListener('click', prev);
 
 // ciclo per aggiungere gli event listeners alle miniature
 for (let i = 0; i < listThumbs.length; i++) {
 	listThumbs[i].addEventListener('click',
-		function() {
+		function () {
 			console.log('cliccata la miniature in posizione ' + i)
 			listHighlighted[activeIndex].classList.remove('active');
 			listThumbs[activeIndex].classList.remove('active');
@@ -98,9 +97,38 @@ for (let i = 0; i < listThumbs.length; i++) {
 		}
 	)
 }
+let interval;
+const btnStart = document.querySelector(".start");
+const btnStop = document.querySelector(".stop");
+btnStart.addEventListener("click", function () {
+	interval = setInterval(next, 2500);
+});
+btnStop.addEventListener("click", function () {
+	clearInterval(interval)
+});
 
-
-
+function write(num) {
+	let toReturn;
+	switch (num) {
+		case 0:
+			toReturn = "Spiderman"
+			break;
+		case 1:
+			toReturn = "Ratchet & Clank";
+			break;
+		case 2:
+			toReturn = "Fortnite";
+			break;
+		case 3:
+			toReturn = "Stray"
+			break;
+		case 4:
+			toReturn = "Avengers";
+		default:
+			break;
+	}
+	return toReturn;
+}
 /*
 btnNext ---> al click fai function() {...}
 btnPrev ---> al click fai function() {...}
